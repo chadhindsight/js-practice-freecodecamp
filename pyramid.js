bb7a01b316354030e039a7335881afc16a1e367b
chad hinds <chadrickj8@gmail.com>
2024-07-28 14:30:08 -0400

    comment out alternate padRow logic for now

--- pyramid.js
+++ pyramid.js
@@ -6,6 +6,16 @@ let inverted = false;
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+// REFACTOR
+// function padRow(rowNumber, rowCount) {
+//     const padLength = rowCount - rowNumber;
+//     const charLength = 2 * rowNumber - 1;
+
+//     const spaces = Array(padLength).fill(" ").map(space => space).join("");
+//     const characters = Array(charLength).fill(character).map(char => char).join("");
+
+//     return spaces + characters + spaces;
+// }
 
 for (let i = 1; i <= count; i++) {
     if (inverted) {
