5885328ac36e5006e21b7586887affd27fb06c3f
chad hinds <chadrickj8@gmail.com>
2024-09-03 12:27:10 -0400

    Update logic for cleanInputString

--- calorieCounter.js
+++ calorieCounter.js
@@ -6,3 +6,10 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 
 let isError = false;
+
+// Remove any '+' or '-' characters from the input string
+function cleanInputString(str) {
+    // need to use the backslash \ character to escape the + symbol because it has a special meaning in regular expressions.
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
