c3edc650ec5fbf698f6c303361864dc284066840
chad hinds <chadrickj8@gmail.com>
2024-09-04 14:11:48 -0400

    Update getCaloriesFromInputs function

--- calorieCounter.js
+++ calorieCounter.js
@@ -30,9 +30,23 @@ function addEntry() {
         <input type="number" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" min="0" />
     `;
 
-    targetInputContainer.innerHTML += HTMLString;
-
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
     addEntryButton.addEventListener("click", addEntry)
 }
 
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+
+    }
+}
 
