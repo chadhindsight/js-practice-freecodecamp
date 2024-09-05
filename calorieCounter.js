27ea7eae903f8723e6987ddb08ec5de726d39302
chad hinds <chadrickj8@gmail.com>
2024-09-05 10:25:42 -0400

    add some more content to the area that displays user calorie info

--- calorieCounter.js
+++ calorieCounter.js
@@ -58,7 +58,12 @@ function calculateCalories(e) {
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}"></span>`;
+    output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+
+  `;
 }
 
 function getCaloriesFromInputs(list) {
