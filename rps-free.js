7cb9da9f2cbf1ccd52d23f12eec3cd40cf0ef6ed
chad hinds <chadrickj8@gmail.com>
2024-09-06 16:35:05 -0400

    Add win conditions for player victory

--- rps-free.js
+++ rps-free.js
@@ -3,4 +3,14 @@ function getRandomComputerResult() {
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+    if (player === "Rock" && computer === "Scissors") return true
+    if (player === "Scissors" && computer === "Paper") return true
+    if (player === "Paper" && computer === "Rock") return true
+
+    return false
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
