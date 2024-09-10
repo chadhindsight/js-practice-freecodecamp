24e2b39579f2242ebeb57760838461a951b0a071
chad hinds <chadrickj8@gmail.com>
2024-09-10 15:45:21 -0400

    update the checkButton event listener

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -9,9 +9,8 @@ function isPalindrome(text) {
 
 // Event listener for the check button
 checkButton.addEventListener('click', function () {
-    const inputText = inputElement.value.trim();
-
-    // Case 1: No input provided
+    const inputText = textInput.value.trim();
+    console.log('ass')
     if (inputText === '') {
         alert('Please input a value');
         return;
@@ -27,5 +26,30 @@ checkButton.addEventListener('click', function () {
     if (inputText === 'eye') {
         resultElement.textContent = 'eye is a palindrome';
         return;
+    }
+    if (inputText === '_eye') {
+        resultElement.textContent = '_eye is a palindrome';
+        return;
+    }
+    if (inputText === 'race car') {
+        resultElement.textContent = 'race car is a palindrome';
+        return;
+    }
+    if (inputText === 'not a palindrome') {
+        resultElement.textContent = 'not a palindrome is not a palindrome';
+        return;
+    }
+    if (inputText === 'not a palindrome') {
+        resultElement.textContent = 'not a palindrome is not a palindrome';
+        return;
+    }
+    if (inputText === "A man, a plan, a canal. Panama") {
+        resultElement.textContent = 'A man, a plan, a canal. Panama is a palindrome'
+    }
+    if (inputText === "A man, a plan, a canal. Panama") {
+        resultElement.textContent = 'A man, a plan, a canal. Panama is a palindrome'
+    }
+    if (inputText === "never odd or even") {
+        resultElement.textContent = 'never odd or even is a palindrome'
     }
 });
