388f87cfd87d8d8ccdd1fcd0afa39a7f6b6996c3
chad hinds <chadrickj8@gmail.com>
2024-09-16 12:02:05 -0400

    BS in between steps :)

--- music-player/script.js
+++ music-player/script.js
@@ -79,5 +79,22 @@ const allSongs = [
     },
 ];
 
+const audio = new Audio();
+let userData = {
+    songs: [...allSongs],
+    currentSong: null,
+    songCurrentTime: 0,
+};
 
+const printGreeting = () => {
+    console.log('Hello there!');
+}
+
+printGreeting();
+
+const printMessage = org => {
+    console.log(`${org} is awesome!`);
+}
+
+printMessage('freeCodeCamp');
 
