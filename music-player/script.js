cbbf32583b1568847a4c233fa6aab4e4f00a3435
chad hinds <chadrickj8@gmail.com>
2024-09-17 11:21:46 -0400

    play song when button gets clicked

--- music-player/script.js
+++ music-player/script.js
@@ -80,7 +80,6 @@ const allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
     songs: [...allSongs],
     currentSong: null,
@@ -89,20 +88,28 @@ let userData = {
 
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
-    audio.src = song.src
-    audio.title = song.title
+    audio.src = song.src;
+    audio.title = song.title;
 
     if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
-        audio.currentTime = 0
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
     }
+    userData.currentSong = song;
+    playButton.classList.add("playing");
+
+    audio.play();
 };
+
+
 
 const renderSongs = (array) => {
     const songsHTML = array
         .map((song) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -118,18 +125,29 @@ const renderSongs = (array) => {
 
     playlistSongs.innerHTML = songsHTML;
 };
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
+
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
         if (a.title < b.title) {
             return -1;
         }
 
+        if (a.title > b.title) {
+            return 1;
+        }
 
+        return 0;
     });
-}
 
-renderSongs(userData?.songs);
+    return userData?.songs;
+};
 
-
-
-
+renderSongs(sortSongs());
