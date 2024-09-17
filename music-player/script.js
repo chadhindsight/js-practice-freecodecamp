80f5591196cb3778ef15625446324ae33dbf3887
chad hinds <chadrickj8@gmail.com>
2024-09-17 11:10:56 -0400

    set the audio.src property equal to song.src

--- music-player/script.js
+++ music-player/script.js
@@ -88,7 +88,9 @@ let userData = {
 };
 
 const playSong = (id) => {
-    const song = userData?.songs.find(song => song.id === id)
+    const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src
+    audio.title = song.title
 };
 
 const renderSongs = (array) => {
