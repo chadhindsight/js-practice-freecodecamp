98ca3824e9dcb84ae7f49d658ae0292eee388859
chad hinds <chadrickj8@gmail.com>
2024-10-04 12:32:13 -0400

    Save last elements in the variables dateInput and descriptionInput respectively.

--- todoApp/script.js
+++ todoApp/script.js
@@ -3,6 +3,13 @@ const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
-// var for the cancel button
 const cancelBtn = document.getElementById("cancel-btn");
 
+// Step 3: Access additional elements
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+
+// Step 4: Access date and description inputs
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
