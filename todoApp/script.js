1dfd8f4001898a8b0ed128832155a4a35647c3bd
chad hinds <chadrickj8@gmail.com>
2024-10-04 13:03:33 -0400

    Add listener for taskForm

--- todoApp/script.js
+++ todoApp/script.js
@@ -4,15 +4,32 @@ const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
-
-// Step 3: Access additional elements
 const discardBtn = document.getElementById("discard-btn");
 const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
-
-// Step 4: Access date and description inputs
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = []
-let currentTask = {}
+const taskData = [];
+let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () =>
+    taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+});
