8f22089ea22f1cdb04126933d796464c7aec2d5d
chad hinds <chadrickj8@gmail.com>
2024-10-04 11:46:00 -0400

    Explain how the canel button is setup

--- todoApp/script.js
+++ todoApp/script.js
@@ -3,5 +3,6 @@ const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+// var for the cancel button
 const cancelBtn = document.getElementById("cancel-btn");
 
