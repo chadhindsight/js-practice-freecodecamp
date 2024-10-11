5e5359a937e40095750058dfcd7f0bcadc8ccb4d
chad hinds <chadrickj8@gmail.com>
2024-10-11 11:50:58 -0400

    Finally, display the form modal with the values of the input fields by using classList to toggle the hidden class on taskForm.

--- todoApp/script.js
+++ todoApp/script.js
@@ -25,6 +25,7 @@ const addOrUpdateTask = () => {
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
+
     updateTaskContainer()
     reset()
 };
@@ -39,13 +40,39 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
-          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `
         }
     );
 };
+
+
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
+    currentTask = taskData[dataArrIndex];
+
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+    taskForm.classList.toggle("hidden");
+
+}
 
 const reset = () => {
     titleInput.value = "";
