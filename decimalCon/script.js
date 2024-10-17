49308ec7b470cdab66b62b41aea9ee030e513e05
chad hinds <chadrickj8@gmail.com>
2024-10-16 20:15:10 -0400

    Within the checkUserInput function, use console.log() to log the value of numberInput to the console.

--- decimalCon/script.js
+++ decimalCon/script.js
@@ -1,7 +1,7 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
-
+// The horrors persist
 const checkUserInput = () => {
     console.log(numberInput.value);
 };
