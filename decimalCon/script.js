e3042258f963ac1583b2e5cba0a2e6ec3828e7d8
chad hinds <chadrickj8@gmail.com>
2024-10-18 12:23:25 -0400

    Set the value property of numberInput to an empty string

--- decimalCon/script.js
+++ decimalCon/script.js
@@ -2,7 +2,9 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => { }
+const decimalToBinary = (input) => {
+    return "01110110";
+};
 
 const checkUserInput = () => {
     if (
@@ -14,7 +16,8 @@ const checkUserInput = () => {
         return;
     }
 
-    console.log(numberInput.value);
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
@@ -24,3 +27,6 @@ numberInput.addEventListener("keydown", (e) => {
         checkUserInput();
     }
 });
+
+// Binary numbers are a base-2 number system. Unlike the base-10 or decimal number system we use every day that uses 10 digits (0-9) to form numbers, the binary number system only has two digits, 0 and 1. In computer science, these binary digits are called bits, and are the smallest unit of data computers can process. For computers, 0 represents false or "off", and 1 represents true or "on".
+
