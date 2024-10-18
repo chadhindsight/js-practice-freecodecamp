20f72d8ae151ff8584cd5832bd054ebe57fff1e5
chad hinds <chadrickj8@gmail.com>
2024-10-18 11:51:48 -0400

    New iteration

--- decimalCon/script.js
+++ decimalCon/script.js
@@ -1,7 +1,10 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
-// The horrors persist
+
 const checkUserInput = () => {
     console.log(numberInput.value);
 };
+
+convertBtn.addEventListener("click", checkUserInput);
+
