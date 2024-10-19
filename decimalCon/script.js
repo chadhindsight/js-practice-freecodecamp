cfb166f14fbc0ff59afb7e221be183261d65ff14
chad hinds <chadrickj8@gmail.com>
2024-10-19 11:06:11 -0400

    Update the last line of the while loop and assign quotient to input.

--- decimalCon/script.js
+++ decimalCon/script.js
@@ -3,9 +3,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    const inputs = []
-    const quotients = []
+    const inputs = [];
+    const quotients = [];
     const remainders = [];
+
+    while (input > 0) {
+        const quotient = Math.floor(input / 2);
+
+        input = quotient;
+    }
 };
 
 const checkUserInput = () => {
