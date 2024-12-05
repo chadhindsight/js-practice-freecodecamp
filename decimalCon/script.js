fd0170fdcdb66559a568a8b4f496ba83afded99d
chadhindsight <chadrickJ8@gmail.com>
2024-12-05 13:48:44 -0500

    Add a class attribute set to "animation-frame"

--- decimalCon/script.js
+++ decimalCon/script.js
@@ -1,32 +1,64 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+const animationData = [
+  {
+    inputVal: 5,
+
+    addElDelay: 1000
+  },
+  {
+    inputVal: 2,
+    addElDelay: 1500
+  },
+  {
+    inputVal: 1,
+    addElDelay: 2000
+  }
+];
 
 const decimalToBinary = (input) => {
-    return "01110110";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+const showAnimation = () => {
+  result.innerText = "Call Stack Animation";
+
+  animationData.forEach((obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" class="animation-frame"></p>
+      `;
+    }, obj.addElDelay);
+  });
 };
 
 const checkUserInput = () => {
-    if (
-        !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0
-    ) {
-        alert("Please provide a decimal number greater than or equal to 0");
-        return;
-    }
+  const inputInt = parseInt(numberInput.value);
 
-    decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = "";
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        checkUserInput();
-    }
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
-
-// Binary numbers are a base-2 number system. Unlike the base-10 or decimal number system we use every day that uses 10 digits (0-9) to form numbers, the binary number system only has two digits, 0 and 1. In computer science, these binary digits are called bits, and are the smallest unit of data computers can process. For computers, 0 represents false or "off", and 1 represents true or "on".
-
