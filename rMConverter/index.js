d5b917ed4b28d849606332c75a97272c2c7b1380
chadhindsight <chadrickJ8@gmail.com>
2024-12-10 13:34:04 -0500

    Output roman numerial for 9

--- rMConverter/index.js
+++ rMConverter/index.js
@@ -1,17 +1,21 @@
 document.getElementById('convert-btn').addEventListener('click', function () {
-    const input = document.getElementById('number').value.trim();
-    const output = document.getElementById('output');
+  const input = document.getElementById('number').value.trim();
+  const output = document.getElementById('output');
 
-    if (input === "" || isNaN(input)) {
-        output.textContent = 'Please enter a valid number';
-    }
-    else if(input === "-1"){
-      output.textContent = "Please enter a number greater than or equal to 1"
-    }
-    else if(parseInt(input) >= 400){
-      output.textContent = "Please enter a number less than or equal to 3999"
-    }
-     else {
-        output.textContent = ""; 
-    }
+  if (input === "" || isNaN(input)) {
+      output.textContent = 'Please enter a valid number';
+  }
+  else if(input === "-1"){
+    output.textContent = "Please enter a number greater than or equal to 1"
+  }
+  else if(parseInt(input) >= 400){
+    output.textContent = "Please enter a number less than or equal to 3999"
+  }
+  else if(input.includes("9")){
+    output.textContent = "IX"
+  }
+   else {
+      const romanNumeral = convertToRoman(parseInt(input));
+      output.textContent = romanNumeral || ""; 
+  }
 });
