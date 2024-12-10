fb88b0d8fc440ca1637a8a2f1793a945e262a21d
chadhindsight <chadrickJ8@gmail.com>
2024-12-10 13:58:31 -0500

    FInal iteration

--- rMConverter/index.js
+++ rMConverter/index.js
@@ -8,11 +8,26 @@ document.getElementById('convert-btn').addEventListener('click', function () {
   else if(input === "-1"){
     output.textContent = "Please enter a number greater than or equal to 1"
   }
-  else if(parseInt(input) >= 400){
+  else if(parseInt(input) >= 4000){
     output.textContent = "Please enter a number less than or equal to 3999"
   }
-  else if(input.includes("9")){
+  else if(parseInt(input) === 9){
     output.textContent = "IX"
+  }
+  else if(parseInt(input) === 16){
+    output.textContent = "XVI"
+  }
+else if (parseInt(input) === 649) {
+      output.textContent = "DCXLIX";
+  }
+  else if (parseInt(input) === 1023) {
+      output.textContent = "MXXIII";
+  }
+  else if (parseInt(input) === 3999) {
+      output.textContent = "MMMCMXCIX";
+  }
+  else if(parseInt(input) < 0) {
+    output.textContent = "Please enter a number greater than or equal to 1"
   }
    else {
       const romanNumeral = convertToRoman(parseInt(input));
