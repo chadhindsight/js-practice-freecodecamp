3bb9411144819508a6be834083168fb9e86ea8dd
chadhindsight <chadrickJ8@gmail.com>
2024-12-13 11:32:22 -0500

    update

--- spamFilter/index.js
+++ spamFilter/index.js
@@ -3,7 +3,7 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex = /[0-9]+ (hundred|thousand|million|billion) dollars/i;
+const dollarRegex = /[0-9]+\s*(hundred|thousand|million|billion)?\s+dollars/i;
 
 const denyList = [helpRegex, dollarRegex];
 
@@ -20,3 +20,4 @@ checkMessageButton.addEventListener("click", () => {
     : "This message does not seem to contain any spam.";
   messageInput.value = "";
 });
+// Free code camp is a dogshit plafform!
